8ca48d59f99790d47d050d1d6826e8e7b7a76e9b
NickShermeister <ncsherman159@gmail.com>
2020-04-07 22:18:59 -0400

    ermmm

--- BAB/bab_starter.py
+++ BAB/bab_starter.py
@@ -6,7 +6,11 @@ import heapq as hq
 import numpy as np
 import itertools
 import math
-counter = itertools.count() 
+import logging
+counter = itertools.count()
+
+SOLVER = 'cvxopt'
+ANS_VAR = "z"
 
 class BBTreeNode():
     def __init__(self, vars = [], constraints = [], objective='', prob=None):
@@ -22,15 +26,15 @@ class BBTreeNode():
         '''
         newprob = pic.Problem.clone(self.prob)
         return BBTreeNode(self.vars, newprob.constraints, self.objective, newprob)
-    
+
     def buildProblem(self):
         '''
         Bulids the initial Picos problem
         '''
         prob=pic.Problem()
-   
-        prob.add_list_of_constraints(self.constraints)    
-        
+
+        prob.add_list_of_constraints(self.constraints)
+
         prob.set_objective('max', self.objective)
         self.prob = prob
         return self.prob
@@ -40,6 +44,9 @@ class BBTreeNode():
         Checks if all variables (excluding the one we're maxing) are integers
         '''
         for v in self.vars[:-1]:
+            logging.info(f'Var round is {round(v.value)}')
+            logging.info(f'Var float is {float(v.value)}')
+
             if v.value == None or abs(round(v.value) - float(v.value)) > 1e-4 :
                 return False
         return True
@@ -73,14 +80,65 @@ class BBTreeNode():
         '''
 
         # these lines build up the initial problem and adds it to a heap
+        logging.info(f'TEST')
         root = self
-        res = root.buildProblem().solve(solver='cvxopt')
-        heap = [(res, next(counter), root)]
+        res = root.buildProblem().solve(solver=SOLVER)
+        heap = [(next(counter), root)]
         bestres = -1e20 # a small arbitrary initial best objective value
         bestnode_vars = root.vars # initialize bestnode_vars to the root vars
 
         #TODO: fill this part in
+        while len(heap) > 0:
+            curr = heap.pop(0)
+            logging.info(f'Current len of heap: {len(heap)}')
+            logging.info(f'counter is: {curr[0]}')
+            logging.info(f'node is: {curr[1]}')
+            logging.info(f'node vals are {[float(x) for x in curr[1].vars]}')
+            if curr[1].is_integral():
+                #save it as the best value if it is better than the bestres
+                logging.info("Is integral")
+                logging.info(f' vars are: {[float(x) for x in curr[1].vars]}')
+                logging.info(f'Compared new bestval is: {curr[1].objective} \n compared to {bestres}')
+                if curr[1].objective > bestres:
+                    bestres = curr[1].objective
+                    bestnode_vars = curr[1].vars
+                    logging.info(f'New bestres is {bestres}')
+                    logging.info(f'New bestnode_vars are: {bestnode_vars}')
 
-        
+            else:
+                #divide and conquer
+                logging.info("Not integral.")
+
+                # Pick a var [loop through]
+                for v in curr[1].vars:
+                    if abs(round(v.value) - float(v.value)) > 1e-4 :
+                        var_to_round = v
+                        break
+
+                # Round up and round down
+                try:
+                    floor = curr[1].branch_floor(var_to_round)
+                    logging.info(f'floor is {floor}')
+                    logging.info(f'floor prob is {floor.prob}')
+                    logging.info(f'floor vars are {[float(x) for x in floor.vars]}')
+                    floor_ans = floor.prob.solve(solver=SOLVER)
+                    heap.append((next(counter), floor))
+                    logging.info(f'newest counter val: {heap[-1][0]}')
+                except:
+                    logging.info("No valid round down.")
+
+                try:
+                    ceil = curr[1].branch_ceil(var_to_round)
+                    logging.info(f'ceil is: {ceil}')
+                    logging.info(f'ceil prob is {ceil.prob}')
+                    logging.info(f'ceil vars are {[float(x) for x in ceil.vars]}')
+                    ceil_ans = ceil.prob.solve(solver=SOLVER)
+                    # #iterate the counter for each of them? I guess? Why is the counter used??
+                    heap.append((next(counter), ceil))
+                    logging.info(f'newest counter val: {heap[-1][0]}')
+                except:
+                    logging.info("No valid round up.")
+
+
+        logging.info(f'Solved everything! bestres: {bestres} \n bestnode_vars: {bestnode_vars} \n\n\n\n\n\n\n\n')
         return bestres, bestnode_vars
- 
